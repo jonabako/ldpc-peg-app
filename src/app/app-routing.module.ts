36a1d3c727e07d0c81e713e52380431fa5353bf6
Jona Bako <jonabako14@yahoo.com>
2024-04-06 16:27:44 +0200

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/ldpc-peg-simulation-tool', pathMatch: 'full' },
-  { path: 'ldpc-peg-simulation-tool', component: HomeComponent }
+  { path: '', component: HomeComponent }
 ];
 
 @NgModule({
